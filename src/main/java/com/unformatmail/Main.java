4d54c323ff2fac3607d81e481987224a67fc94f8
Denis <114498387+Runciterr@users.noreply.github.com>
2024-01-14 23:49:42 +0300

    Update Main.java

--- src/main/java/com/unformatmail/Main.java
+++ src/main/java/com/unformatmail/Main.java
@@ -2,6 +2,6 @@ package com.unformatmail;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello AQA!");
+        System.out.println("Hello AQA!!!!!");
     }
 }
